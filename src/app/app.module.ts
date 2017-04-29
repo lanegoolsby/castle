8827c77d71a71d15dda0392cc7655a2bab301ff8
Lane Goolsby <Lane Goolsby>
2017-04-29 17:24:43 -0400

    Renaming Settings path to match patterns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { DevicesComponent } from './components/devices/devices.comp';
-import { SettingsComponent } from './components/settings_comp/settings.component';
+import { SettingsComponent } from './components/settings.comp/settings.component';
 import { KeepComponent } from './components/common/keep/keep.comp';
 import { RoomComponent } from './components/common/room/room.comp';
 import { ThingComponent } from './components/common/thing/thing.comp';
